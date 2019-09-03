2a669abc2b1e1fb359eecf75fe7695ac64cd8d74
enderson <enderson19952009@gmail.com>
2019-09-03 17:54:11 -0400

    do some prubes

--- components/js/building.js
+++ components/js/building.js
@@ -1,9 +1,22 @@
 define(['angular', 'require'], function(angular, require) {
     'use strict';
-    HelloWorldController.$inject = ['$scope'];
-    function HelloWorldController($scope) {
-    this.$onInit = () => {
+    HelloWorldController.$inject = ['$scope', 'maDataPointTags', '$state', '$stateParams', 'maPoint'];
+    function HelloWorldController($scope, maDataPointTags, $state, $stateParams, maPoint) {
+        this.$onInit = () => {
             this.helloWorld = 'Hola Mango';
+            this.refreshSites ();
+        };
+        this.refreshSites = () => {
+            return maDataPointTags
+            .buildQuery('Building')
+            .query()
+            .then(values => {
+                this.building = values.sort();
+                this.building.unshift('ALL');
+                this.building.unshift('Nuevo');
+                this.helloWorld = 'cambio';
+                console.log(this.building)
+            })
         };
     }
     return {
@@ -12,4 +25,4 @@ define(['angular', 'require'], function(angular, require) {
         templateUrl: require.toUrl('../html/building.html')
     };
         
-    });
+});
--- components/js/overview.js
+++ components/js/overview.js
@@ -1,15 +1,25 @@
 define(['angular', 'require'], function(angular, require) {
-'use strict';
-HelloWorldController.$inject = ['$scope'];
-function HelloWorldController($scope) {
-this.$onInit = () => {
-        this.helloWorld = 'Hola Mango';
+    'use strict';
+    HelloWorldController.$inject = ['$scope'];
+    function HelloWorldController($scope) {
+        this.$onInit = () => {
+                this.helloWorld = 'Hola Mango';
+                this.refreshSites ();
+            };
+
+        this.refreshSites = () => {
+            return maDataPointTags
+            .buildQuery('building')
+            .query()
+            .then(values => {
+            this.building = values.sort();
+        })
+        };
+    }
+    return {
+        bindings: {},
+        controller: HelloWorldController,
+        templateUrl: require.toUrl('../html/overview.html')
     };
-}
-return {
-    bindings: {},
-    controller: HelloWorldController,
-    templateUrl: require.toUrl('../html/overview.html')
-};
-    
+        
 });
