2cd43c2631879656be3e0d19813c7d588b60dcf4
enderson <enderson19952009@gmail.com>
2019-09-04 15:49:19 -0400

    overview 90%

--- components/js/building.js
+++ components/js/building.js
@@ -1,22 +1,52 @@
 define(['angular', 'require'], function(angular, require) {
     'use strict';
     HelloWorldController.$inject = ['$scope', 'maDataPointTags', '$state', '$stateParams', 'maPoint'];
-    function HelloWorldController($scope, maDataPointTags, $state, $stateParams, maPoint) {
+    function HelloWorldController($scope, maDataPointTags, $state, $stateParams, maPoint){
         this.$onInit = () => {
             this.helloWorld = 'Hola Mango';
             this.refreshSites ();
         };
+
         this.refreshSites = () => {
             return maDataPointTags
-            .buildQuery('Building')
+            .buildQuery('building')
             .query()
             .then(values => {
                 this.building = values.sort();
-                this.building.unshift('ALL');
-                this.building.unshift('Nuevo');
-                this.helloWorld = 'cambio';
-                console.log(this.building)
+                this.buildingPoints = {};
+
+                this.dataPoint();
             })
+        };
+
+        this.dataPoint = () => {
+            let queryBuilder = maPoint.buildQuery();
+
+            return queryBuilder
+            .limit(1000)
+            .query()
+            .then((points) => { 
+                this.orderPoints(points);
+               
+            });
+        };
+
+        this.orderPoints = (points) => {
+            console.log(points)
+
+            this.building.forEach(building => {
+                this.buildingPoints[building] = {
+                    'powerTotal' : this.filterByNameAndBuilding(points, 'powerTotal',building),
+                };
+                
+            });
+           console.log(this.buildingPoints)
+        };
+
+        this.filterByNameAndBuilding = (points, name, building) => {
+            return points.filter(point => {
+                return point.name == name && point.tags.building == building;
+            })[0];
         };
     }
     return {
--- components/js/overview.js
+++ components/js/overview.js
@@ -1,19 +1,52 @@
 define(['angular', 'require'], function(angular, require) {
     'use strict';
-    HelloWorldController.$inject = ['$scope'];
-    function HelloWorldController($scope) {
+    HelloWorldController.$inject = ['$scope', 'maDataPointTags', '$state', '$stateParams', 'maPoint'];
+    function HelloWorldController($scope, maDataPointTags, $state, $stateParams, maPoint){
         this.$onInit = () => {
-                this.helloWorld = 'Hola Mango';
-                this.refreshSites ();
-            };
+            this.helloWorld = 'Hola Mango';
+            this.refreshSites ();
+        };
 
         this.refreshSites = () => {
             return maDataPointTags
             .buildQuery('building')
             .query()
             .then(values => {
-            this.building = values.sort();
-        })
+                this.building = values.sort();
+                this.buildingPoints = {};
+
+                this.dataPoint();
+            })
+        };
+
+        this.dataPoint = () => {
+            let queryBuilder = maPoint.buildQuery();
+
+            return queryBuilder
+            .limit(1000)
+            .query()
+            .then((points) => { 
+                this.orderPoints(points);
+               
+            });
+        };
+
+        this.orderPoints = (points) => {
+            console.log(points)
+
+            this.building.forEach(building => {
+                this.buildingPoints[building] = {
+                    'powerTotal' : this.filterByNameAndBuilding(points, 'powerTotal',building),
+                };
+                
+            });
+           console.log(this.buildingPoints)
+        };
+
+        this.filterByNameAndBuilding = (points, name, building) => {
+            return points.filter(point => {
+                return point.name == name && point.tags.building == building;
+            })[0];
         };
     }
     return {
