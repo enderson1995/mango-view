4f2bfa8bf1ca98ecb5cf01faa7165e6abaf86ed3
enderson <enderson19952009@gmail.com>
2019-09-05 15:08:01 -0400

    some changes

--- components/js/building.js
+++ components/js/building.js
@@ -2,45 +2,63 @@ define(['angular', 'require'], function(angular, require) {
     'use strict';
     HelloWorldController.$inject = ['$scope', 'maDataPointTags', '$state', '$stateParams', 'maPoint'];
     function HelloWorldController($scope, maDataPointTags, $state, $stateParams, maPoint){
+
         this.$onInit = () => {
             this.helloWorld = 'Hola Mango';
             this.refreshSites ();
         };
 
         this.refreshSites = () => {
+            //encontrar los tags asociados a los nombres de los edificios
             return maDataPointTags
-            .buildQuery('building')
-            .query()
-            .then(values => {
-                this.building = values.sort();
-                this.buildingPoints = {};
-
-                this.dataPoint();
-            })
+                .buildQuery('building')
+                .query()
+                .then(values => {
+                    this.building = values.sort();
+                    
+                    this.siteChanged();
+                })
         };
 
+        this.siteChanged = () => {
+            //Recargar la pagina cuando se selecciona algun elemento
+            this.buildingPoints = {};
+            $stateParams.site = this.site;
+            $state.go('.', $stateParams, {location: 'replace', notify: false});
+            this.sites = [];
+
+            //Establecer valor de inicio por defecto
+            if(!this.site){
+                this.site = this.building [0];
+            }
+
+            //Variable de control de elementos a mostrar
+            this.sites.unshift(this.site);
+            this.dataPoint();
+        };
+
+        //Traer los datos de mango
         this.dataPoint = () => {
             let queryBuilder = maPoint.buildQuery();
 
             return queryBuilder
-            .limit(1000)
-            .query()
-            .then((points) => { 
-                this.orderPoints(points);
-               
-            });
+                .limit(1000)
+                .query()
+                .then((points) => { 
+                    
+                    this.orderPoints(points);               
+                });
         };
 
         this.orderPoints = (points) => {
-            console.log(points)
-
-            this.building.forEach(building => {
+            
+            //Crear variables a mostrar
+            this.sites.forEach(building => {
                 this.buildingPoints[building] = {
-                    'powerTotal' : this.filterByNameAndBuilding(points, 'powerTotal',building),
+                    'powerTotal' : this.filterByNameAndBuilding(points, 'powerTotal',this.site),
                 };
                 
             });
-           console.log(this.buildingPoints)
         };
 
         this.filterByNameAndBuilding = (points, name, building) => {
