873eddc71038b03576b9aab3304c57be3aa42523
enderson <enderson19952009@gmail.com>
2019-09-09 13:29:02 -0400

    tables ready
    make svg

--- components/js/building.js
+++ components/js/building.js
@@ -1,10 +1,10 @@
 define(['angular', 'require'], function(angular, require) {
     'use strict';
-    HelloWorldController.$inject = ['$scope', 'maDataPointTags', '$state', '$stateParams', 'maPoint'];
-    function HelloWorldController($scope, maDataPointTags, $state, $stateParams, maPoint){
+    HelloWorldController.$inject = ['$scope', 'maDataPointTags', '$state', '$stateParams', 'maPoint', 'maUiDateBar'];
+    function HelloWorldController($scope, maDataPointTags, $state, $stateParams, maPoint, maUiDateBar){
 
         this.$onInit = () => {
-            this.helloWorld = 'Hola Mango';
+            this.maUiDateBar = maUiDateBar;
             this.refreshSites ();
         };
 
@@ -54,6 +54,7 @@ define(['angular', 'require'], function(angular, require) {
             
             //Crear variables a mostrar
             this.sites.forEach(building => {
+
                 this.buildingPoints[building] = {
                     'power1' : this.filterByNameAndBuildingAndLine(points, 'Power',this.site, '1'),
                     'power2' : this.filterByNameAndBuildingAndLine(points, 'Power',this.site, '2'),
@@ -61,6 +62,7 @@ define(['angular', 'require'], function(angular, require) {
                     'powerTotal' : this.filterByNameAndBuilding(points, 'powerTotal',this.site),
                     'energyTotal' : this.filterByNameAndBuilding(points, 'energyTotal',this.site),
                     'costTotal' : this.filterByNameAndBuilding(points, 'costTotal',this.site),
+                    'frecuency' : this.filterByNameAndBuilding(points, 'Frecuency',this.site),
                     'maximumValue' : this.filterByNameAndBuilding(points, 'maximumValue',this.site),
                     'vLineNeutral1' : this.filterByNameAndBuildingAndLine(points, 'vLineToNeutral',this.site,'1'),
                     'vLineNeutral2' : this.filterByNameAndBuildingAndLine(points, 'vLineToNeutral',this.site,'2'),
@@ -79,8 +81,14 @@ define(['angular', 'require'], function(angular, require) {
                     'fp3' : this.filterByNameAndBuildingAndLine(points, 'FP',this.site,'3'),
                     'fpAverage' : this.filterByNameAndBuildingAndLine(points, 'FP',this.site,'Average'),
                 };
-                console.log(this.buildingPoints)
-                console.log(points)
+                
+                this.energyTotal = this.filterByNameAndBuilding(points, 'energyTotal', this.site);
+                this.costTotal = this.filterByNameAndBuilding(points, 'costTotal', this.site);
+                this.vLineToNeutralAverage = this.filterByNameAndBuildingAndLine(points, 'vLineToNeutral',this.site,'Average');
+                this.vLineToLineAverage = this.filterByNameAndBuildingAndLine(points, 'vLineToLine',this.site,'Average');
+                this.currentTotal = this.filterByNameAndBuildingAndLine(points, 'Current',this.site,'Total');
+                this.powerTotal = this.filterByNameAndBuilding(points, 'powerTotal',this.site);
+                
             });
         };
 
